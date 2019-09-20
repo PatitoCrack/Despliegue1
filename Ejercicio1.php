aec68fb7784f5f7576f579be97f84dfc0e8d4950
William Azuaga Rebull <wiazre@floridauniversitaria.es>
2019-09-20 11:17:01 +0200

    hola

---
+++ Ejercicio1.php
@@ -0,0 +1,43 @@
+<?php
+class libro{
+    public $autor = 'paco';
+    public $anyo_de_publicacion = '1322';
+    public $editorial = 'yo quese bro';
+    public $titulo = 'owo';
+    public $num_pags = '3';
+
+    function __construct(){
+
+    }
+
+    function hola(){
+        echo $this->autor . '<br>';
+        echo $this->anyo_de_publicacion . '<br>';
+        echo $this->editorial . '<br>';
+        echo $this->titulo . '<br>';
+        echo $this->num_pags . '<br>';
+    }
+
+    
+
+
+}
+
+$a = new libro();
+$a ->hola();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+
+   ?> 
+</body>
+</html>
---
+++ Ejercicio2.php
@@ -0,0 +1,12 @@
+<?php
+$ciudades=['sueca','cullera','riola'];
+$provincia=['valencia','madrid','barcelona'];    
+var_dump($provincia);
+
+for($i=0; $i<sizeof($ciudades); $i++) $var2[]=array($ciudades[$i], $provincia[$i]); 
+echo '<br>' ;
+var_dump($var2);
+
+   
+
+
